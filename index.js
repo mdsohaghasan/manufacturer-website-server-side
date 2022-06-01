89636826a25e9fe57c695962560129e07c31a45e
Sohag Hasan <mailsohaghasan@gmail.com>
2022-06-01 12:10:44 +0600

    fix problem

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json())
 
 
 // MONGODB CONNECTION
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pajtj.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -33,7 +33,7 @@ function verifyJWT(req, res, next) {
     });
 }
 
-// DATABASE CONNECTION
+// DATABASE CONNECT
 async function run() {
     try {
         await client.connect();
